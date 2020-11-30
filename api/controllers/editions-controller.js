37154ca2762d1d6656fedbffcccb4dfd68842e07
Lokor4ik <lokorinfantem@gmail.com>
2020-11-30 15:20:41 +0200

    added database query that returns editions

--- api/controllers/editions-controller.js
+++ api/controllers/editions-controller.js
@@ -1,52 +1,34 @@
 const db = require('../models');
 
 const {
-  authors: Author,
-  articles: Article,
   editions: Edition
 } = db;
 
 exports.findAll = async (req, res) => {
   try {
-    const data = await Edition.findAll({
-      include: [
-        {
-          model: Article,
-          as: 'articles',
-          attributes: ['id', 'title', 'body', [db.sequelize.fn('COUNT', 'authors.id'), 'commentsCounter']],
-
-          include: [
-            {
-              model: Author,
-              as: 'authors',
-              attributes: ['id'],
-              through: {
-                attributes: []
-              },
-              group: ['authors.id'],
-            }
-          ],
-          group: ['editions.id', 'articles.id', 'authors.id'],
-        }
-      ],
-      group: ['editions.id', 'articles.id', 'authors.id'],
+    const countEditions = await Edition.findAll({
+      attributes: ['id', 'title', [db.sequelize.literal(`(
+        SELECT COUNT (articles.id)
+        FROM          articles
+        WHERE articles.edition_id = editions.id
+        AND (
+          SELECT COUNT (author_article.article_id)
+          FROM          author_article
+          WHERE author_article.article_id = articles.id
+          ) > 1
+        )`), 'articles_count']],
+      raw: true
     });
 
-    const newData = await db.sequelize.query(`
-          SELECT  ed.id
-          FROM    editions as ed
-          WHERE  EXISTS
-          ( 
-            SELECT art.id,art.title,art.body
-FROM articles as art
-WHERE 
-(
-  array_length(array(SELECT author_article.author_id FROM 
-  author_article WHERE author_article.article_id = art.id), 1) > 2
-)
-  AND art.edition_id = ed.id
-          )
-    `);
+    const reduceArticlesCount = Math.round(
+      countEditions.reduce((acc, curr) => (
+        acc + Number(curr.articles_count)
+      ), 0) / countEditions.length
+    );
+
+    const data = countEditions.filter(editionItem => (
+      editionItem.articles_count >= reduceArticlesCount
+    ));
 
     res.send(data);
   } catch (err) {
