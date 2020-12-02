d5ea9b3cfdbd10b1b39b4c197092ab2c5a49acac
Lokor4ik <lokorinfantem@gmail.com>
2020-12-02 16:55:30 +0200

    added method for articleFindOne

--- services/ArticleDataService.js
+++ services/ArticleDataService.js
@@ -4,6 +4,10 @@ class ArticleAPI {
   static getAllArticles({ page, size }) {
     return http.get(`/articles?page=${page}&size=${size}`);
   }
+
+  static getCurrentArticle(id) {
+    return http.get(`/article?id=${id}`);
+  }
 }
 
 export default ArticleAPI;
