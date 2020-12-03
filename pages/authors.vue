c62a41abd27175c64cb002d239d310a8966dcbf8
Lokor4ik <lokorinfantem@gmail.com>
2020-12-03 16:22:46 +0200

    moved the logic to the container component

--- pages/authors.vue
+++ pages/authors.vue
@@ -1,17 +1,38 @@
 <template>
   <section class="authors">
     <NavAuthorsInputs />
-    <NavAuthorsReports />
+    <NavAuthorsReports
+      v-show="allMonths.length"
+      :allMonths="allMonths"
+      :authors="authors"
+      :handleChange="handleChange"
+    />
   </section>
 </template>
 
 <script>
 export default {
+  computed: {
+    allMonths() {
+      return this.$store.getters["authors/allMonths"];
+    },
+    authors() {
+      return this.$store.getters["authors/authors"];
+    },
+    error() {
+      return this.$store.getters["authors/error"];
+    },
+  },
+  methods: {
+    async handleChange(month) {
+      await this.$store.dispatch("authors/fetchAuthors", { month });
+    },
+  },
 };
 </script>
 
 <style>
-.authors{
+.authors {
   text-align: center;
 }
 </style>
