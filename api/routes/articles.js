0d88c3b31105d826e5ed5035c2b21b3d2bc944b6
Lokor4ik <lokorinfantem@gmail.com>
2020-12-14 16:49:16 +0200

    added get request for count articles

--- api/routes/articles.js
+++ api/routes/articles.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const articles = require('../controllers/articles-controller');
 
+router.get('/api/countArticle', articles.countAll);
 router.get('/api/articles', articles.findAll);
 router.get('/api/article', articles.findOne);
 
