b05d9e4f6321e75ebc40cd46234d75c3a79c99a8
Lokor4ik <lokorinfantem@gmail.com>
2020-12-14 16:49:42 +0200

    empty markup

--- components/NavTabs/NavTabsEditions.vue
+++ components/NavTabs/NavTabsEditions.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="editions">
     <div v-if="!error" class="tab-pane__editions">
-    {{editions}}
+      
     </div>
     <div v-if="error" class="err">
       <h3 class="error-title">{{ error }}</h3>
