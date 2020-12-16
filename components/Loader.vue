bddca046cd7968843c06f19c9f7a38a7e41207e2
Lokor4ik <lokorinfantem@gmail.com>
2020-12-16 16:23:39 +0200

    added circular loader

---
+++ components/Loader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="loader-wrapper">
+    <v-progress-circular
+      :size="70"
+      :width="7"
+      color="green"
+      indeterminate
+    ></v-progress-circular>
+  </div>
+</template>
+
+<style lang="scss">
+.loader-wrapper { 
+display: flex;
+justify-content: center;
+}
+</style>
