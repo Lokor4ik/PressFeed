4e103ca16892ab4c81e941a94e456a82379e5813
Lokor4ik <lokorinfantem@gmail.com>
2020-12-17 16:24:41 +0200

    changed some properties

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,7 +40,7 @@ export default {
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     '@nuxtjs/axios',
-    '@nuxtjs/vuetify'
+    ['@nuxtjs/vuetify', { treeShake: true }]
   ],
 
   serverMiddleware: {
@@ -48,5 +48,6 @@ export default {
   },
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
+    extractCSS: true
   }
 };
