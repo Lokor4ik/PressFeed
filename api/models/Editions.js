390696d11c2274fa006e0701a54ab931cd9ffcf0
Lokor4ik <lokorinfantem@gmail.com>
2020-12-21 16:00:39 +0200

    changed the name of model

--- api/models/Editions.js
+++ api/models/Editions.js
@@ -1,9 +1,9 @@
 module.exports = (sequelize, Sequelize) => {
-  const Newspapers = sequelize.define('editions', {
+  const Editions = sequelize.define('editions', {
     title: {
       type: Sequelize.STRING
     }
   });
 
-  return Newspapers;
+  return Editions;
 };
