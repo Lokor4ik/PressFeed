04bc5120fb7f9313b22b63e54207566b97f1eaee
Lokor4ik <lokorinfantem@gmail.com>
2020-12-21 14:58:08 +0200

    added query for editions controller

--- api/controllers/editions-controller.js
+++ api/controllers/editions-controller.js
@@ -1,59 +1,14 @@
 const db = require('../models');
 const { calculateAvarageArticles } = require('../utils/helpers');
 
-const {
-  editions: Edition,
-  articles: Article,
-  authors: Author
-} = db;
+/* const {
+  editions: Edition
+} = db; */
 
 exports.findAll = async (req, res) => {
   try {
     /* const editions = await Edition.findAll({
-      include: [
-        {
-          model: Article,
-          as: 'articles'
-        }
-      ]
-    }); */
-
-    /* const avarageArticlesCount = calculateAvarageArticles(editions);
-
-    const data = editions.filter(editionItem => (
-      editionItem.articles_count >= avarageArticlesCount
-    )); */
-
-    const editions = await db.sequelize.query(`
-      SELECT       e.title, e.id,  COUNT(DISTINCT au.id) as au_count
-      FROM         editions e
-      INNER JOIN   articles  ar
-        ON         ar.edition_id = e.id
-      INNER JOIN   author_article aa 
-        ON ar.id = aa.article_id
-      INNER JOIN   authors au 
-        ON         aa.author_id = au.id
-      GROUP BY     e.id, e.title
-      HAVING
-        COUNT(au.id) > 1 
-      ORDER BY e.id
-    `);
-    /*
-    select *
-    from (
-       select gid, count(*) as tmpcount from gd group by gid
-    ) as tmp
-    where tmpcount > 10;
-    */
-    res.send(editions[0]);
-  } catch (err) {
-    res.status(500).send({
-      message: err.message || 'Some error occurred while retrieving tutorials'
-    });
-  }
-};
-/*
-[db.sequelize.literal(`(
+      attributes: ['id', 'title', [db.sequelize.literal(`(
         SELECT COUNT (articles.id)
         FROM          articles
         WHERE articles.edition_id = editions.id
@@ -62,5 +17,35 @@ exports.findAll = async (req, res) => {
           FROM          author_article
           WHERE author_article.article_id = articles.id
           ) > 1
-        )`), 'articles_count']
-*/
+        )`), 'articles_count']]
+    }); */
+
+    const [editions] = await db.sequelize.query(`
+      SELECT     e.title, e.id, COUNT (ar.id) as articles_count
+      FROM       editions e
+      INNER JOIN articles ar
+        ON       ar.edition_id = e.id
+      WHERE (
+        SELECT COUNT (aa.article_id)
+        FROM          authors au
+        INNER JOIN    author_article  aa
+          ON          aa.author_id  = au.id
+          AND         aa.article_id = ar.id
+      ) > 1
+      GROUP BY   e.id, e.title
+      ORDER BY   articles_count DESC
+    `);
+
+    const avarageArticlesCount = calculateAvarageArticles(editions);
+
+    const data = editions.filter(editionItem => (
+      editionItem.articles_count >= avarageArticlesCount
+    ));
+
+    res.send(data);
+  } catch (err) {
+    res.status(500).send({
+      message: err.message || 'Some error occurred while retrieving tutorials'
+    });
+  }
+};
